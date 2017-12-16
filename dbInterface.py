1e87db5080da6ae211b5739beb5cd76516973e7e
Ryan Liu <dio.ryanliu@hotmail.com>
2017-12-16 03:16:18 -0800

    Fixing indentation issues

--- dbInterface.py
+++ dbInterface.py
@@ -1,19 +1,19 @@
 class History(object):
-	def __init__(self, name, userID):
-		self.name = name
-		self.userID = userID
-		self.history = []
+    def __init__(self, name, userID):
+        self.name = name
+        self.userID = userID
+        self.history = []
 
-	def logMessage(self, lastMessage):
-		if len(self.history) > 10:
-			self.history.pop()
-		self.history.append(lastMessage)
+    def logMessage(self, lastMessage):
+        if len(self.history) > 10:
+            self.history.pop()
+        self.history.append(lastMessage)
 
-	def getLastMessages(self, num):
-		historyWanted = int(num)
+    def getLastMessages(self, num):
+        historyWanted = int(num)
 
-		if historyWanted > len(self.history):
-			historyWanted = len(self.history)
+        if historyWanted > len(self.history):
+            historyWanted = len(self.history)
 
-		lastMessages = self.history[--historyWanted:]
-		return lastMessages
+        lastMessages = self.history[--historyWanted:]
+        return lastMessages
--- main.py
+++ main.py
@@ -7,43 +7,50 @@ import platform
 import dbInterface
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-discordBot = Bot(description="TestBot", command_prefix="!", pm_help = True)
+discordBot = Bot(description="TestBot", command_prefix="!", pm_help=True)
 Users = dict()
 
-def getToken():
-	data = json.load(open('token.json'))
-	return data["token"]
 
-# This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
+def getToken():
+    data = json.load(open('token.json'))
+    return data["token"]
+
+
+# This is what happens everytime the bot launches.
 @discordBot.event
 async def on_ready():
-	print('Logged in as')
-	print(discordBot.user.name)
-	print(discordBot.user.id)
+    print('Logged in as')
+    print(discordBot.user.name)
+    print(discordBot.user.id)
 
-	for member in discordBot.get_all_members():
-		Users[member.name] = dbInterface.History(member.name, member.id)
-		Users[member.name].logMessage("ASDF")
+    for member in discordBot.get_all_members():
+        Users[member.name] = dbInterface.History(member.name, member.id)
+        Users[member.name].logMessage("ASDF")
+
 
 @discordBot.event
 async def on_message(message):
-	if message.author == discordBot.user:
-		return
+    if message.author == discordBot.user:
+        return
 
-	Users[message.author.name].logMessage(message.content)
+    Users[message.author.name].logMessage(message.content)
 
-	await discordBot.process_commands(message)
+    await discordBot.process_commands(message)
+
 
 @discordBot.command()
 async def ping(*args):
-	await discordBot.say(":ping_pong: Pong!")
+    await discordBot.say(":ping_pong: Pong!")
+
 
 @discordBot.command()
 async def pong(*args):
-	await discordBot.say(":ping_pong: Ping!")
+    await discordBot.say(":ping_pong: Ping!")
 
-@discordBot.command(pass_context = True)
+
+@discordBot.command(pass_context=True)
 async def history(ctx, num):
-	await discordBot.say(Users[ctx.message.author.name].getLastMessages(num))
+    await discordBot.say(Users[ctx.message.author.name].getLastMessages(num))
+
 
 discordBot.run(getToken())
